5d54f1a4c14d9fb68318400304f325e9c443fdec
Gabriel Moura <gabriel.mourare@protonmail.com>
2022-01-14 17:20:32 -0300

    First commit

---
+++ PlaceOrder/Entities/Order.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlaceOrder.Entities;
+using PlaceOrder.Enums;
+using System.Globalization;
+
+namespace PlaceOrder.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Client Client { get; set; }
+
+
+        public Order()
+        {
+
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+            foreach(OrderItem item in Items)
+            {
+                sum += item.Price;             
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order Items: ");
+
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            sb.AppendLine("Total Price: " + Total().ToString("F2", CultureInfo.InvariantCulture));
+
+            return sb.ToString();
+                
+        }
+    }
+}
---
+++ PlaceOrder/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using PlaceOrder.Entities;
+using PlaceOrder.Enums;
+
+namespace PlaceOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrar com dados do cliente:
+
+            Console.WriteLine("Enter client data: ");
+            Console.WriteLine("Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Email: ");
+            string email = Console.ReadLine();
+            Console.WriteLine("Birthdate: ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            //Instanciar o objeto cliente com os dados fornecidos;
+            Client client = new Client(name, email, birthDate);
+            
+
+            //entrar com dados do pedido;
+
+            Console.WriteLine("Enter order data: ");
+
+            Console.WriteLine("Status: ");
+
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.WriteLine("How many items to this order?");
+            int quantityItems = int.Parse(Console.ReadLine());
+
+
+            for(var i = 0; i <= quantityItems; i++)
+            {
+                Console.WriteLine("Enter item #" + i + " data:");
+                Console.WriteLine("Product Name: ");
+
+                string productName = Console.ReadLine();
+
+                Console.WriteLine("Product Price: ");
+                double productPrice = double.Parse(Console.ReadLine());
+                Console.WriteLine("Quantity: ");
+                int productQuantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(productName, productPrice, productQuantity);
+
+                OrderItem orderItem = new OrderItem(quantityItems, productPrice, product);
+
+                order.AddItem(orderItem);
+            }
+
+            Console.Write( "ORDER SUMMARY: ");
+            Console.WriteLine(order);
+            
+            
+        }
+    }
+}
